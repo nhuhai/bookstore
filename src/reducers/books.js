5285319689fa38ca9b40362da66c4feda3fd279f
Hai Nguyen <nhuhai.h.nguyen@gmail.com>
2018-04-06 09:49:44 -0700

    add books reducer and root reducer

---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' }, // eslint-disable-line
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquen Ruby' }
+  ];
+}
