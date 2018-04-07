dab95bd6c82422787eb71b8c85ceb4bc15a0f4d6
Hai Nguyen <nhuhai.h.nguyen@gmail.com>
2018-04-06 18:29:21 -0700

    display list of books, add BookList container, connect to store

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './app.css';
+import BookList from '../../containers/book-list/BookList';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <BookList />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 // registerServiceWorker();
